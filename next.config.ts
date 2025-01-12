dbbef58d593f743bbb0452d05ef311787ae36740
Will Lopes <willian1999lopes@gmail.com>
2025-01-12 03:05:31 -0300

    commit versão igual a primeira

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
+
 const nextConfig: NextConfig = {
-  /* config options here */
+//   /* config options here */
+  // output: 'export', // Configura exportação estática
+  // images: {
+  //   unoptimized: true, // Desativa otimização de imagens (necessária para exportação)
+  // },
+  // basePath: isProd ? '/apvs_page' : '', // Use o nome exato do repositório
+  // assetPrefix: isProd ? '/apvs_page/' : '',
 };
 
 export default nextConfig;
